2982951d585cb395e96939300d97bb9900ab1ac2
abdelrahman <104309289+abdo6999@users.noreply.github.com>
2023-01-28 20:46:29 +0200

    add svg and fix staff

--- src/app/home/skills/card/card.component.ts
+++ src/app/home/skills/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, AfterViewInit } from '@angular/core';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeUrl } from "@angular/platform-browser";
 
@@ -7,13 +7,16 @@ import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeUrl } from "@angular/platf
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardSkillsComponent {
+export class CardSkillsComponent implements AfterViewInit {
   @Input() front!: IconProp|undefined;
   @Input() svg!: string;
   @Input() back!: string;
   @ViewChild('font') public font!: ElementRef;
-  svgData:SafeHtml= this.sanitizer.bypassSecurityTrustHtml(this.svg);;
   constructor(private sanitizer: DomSanitizer) {}
+  svgData!:SafeHtml ;
+  ngAfterViewInit(): void {
+    this.svgData = this.sanitizer.bypassSecurityTrustHtml(this.svg);;
+  }
 
   
 }
